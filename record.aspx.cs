b9c4af4c60a896fa920c7cd084e41ec6babf5086
pradeep negi <pradeep1383@gmail.com>
2022-05-05 18:23:25 +0530

    first commit

---
+++ record.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class record : System.Web.UI.Page
+{
+
+    SqlConnection con = new SqlConnection(@"Data Source=104.42.188.59;Initial Catalog=mydb;User ID=admini;Password=123@password");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        con.Open();
+        //Response.Write("connection ok");
+
+        string s = "select * from student";
+        SqlDataAdapter da = new SqlDataAdapter(s, con);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        recordgridview.DataSource = ds;
+        recordgridview.DataBind();
+        con.Close();
+    }
+}
